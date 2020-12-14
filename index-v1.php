491d3f7c07bae86e1cd8fb30a6d3730998c3b0c8
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-12-13 16:41:08 -0800

    cleanup index-v1

--- index-v1.php
+++ index-v1.php
@@ -7,14 +7,15 @@
 echo '<h1>SDK1: Uncomment Code to see more</h1>';
 
 require_once __DIR__ . '/vendor/autoload.php';
- \Cloudinary::config(array(
+ Cloudinary::config(array(
     'cloud_name' => 'CLOUD_NAME',
     'api_key' => 'API_KEY',
-   'api_secret' => 'API_SECRET'
+    'api_secret' => 'API_SECRET'
 ));
 
  // Upload API
-//$upload = new \Cloudinary\Uploader();
+//use Cloudinary\Uploader;
+//$upload = new Uploader();
 //echo '<pre>';
 //print_r($upload->upload("https://cloudinary-training.github.io/cld-php-migration/images/cloudinary_icon_blue.png",
 //['public_id'=>'cloudinary_icon_blue']));
@@ -22,11 +23,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 // Admin API
-//$api = new \Cloudinary\Api();
+//use Cloudinary\Api;
+//$api = new Api();
 //echo '<pre>';
 //print_r($api->resources());
 //echo '</pre>';
-//
+
 //$url =  cloudinary_url(
 //    "sample",
 //    [
