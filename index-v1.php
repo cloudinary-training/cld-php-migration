b63fac8acb2a916845df8e7ab9150b045f3e5967
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-12-14 18:44:05 -0800

    add license, update readme, move use to top in php files, update migration image

--- index-v1.php
+++ index-v1.php
@@ -7,76 +7,77 @@
 echo '<h1>SDK1: Uncomment Code to see more</h1>';
 
 require_once __DIR__ . '/vendor/autoload.php';
- Cloudinary::config(array(
-    'cloud_name' => 'CLOUD_NAME',
-    'api_key' => 'API_KEY',
-    'api_secret' => 'API_SECRET'
+
+use \Cloudinary\Uploader;
+use \Cloudinary\Api;
+
+Cloudinary::config(array(
+    'cloud_name' => 'dec-2020-test',
+    'api_key' => '552265519859882',
+    'api_secret' => 'zrs73txUXMnsO93hWxeiRWpTOFw'
 ));
 
- // Upload API
-//use Cloudinary\Uploader;
-//$upload = new Uploader();
-//echo '<pre>';
-//print_r($upload->upload("https://cloudinary-training.github.io/cld-php-migration/images/cloudinary_icon_blue.png",
-//['public_id'=>'cloudinary_icon_blue']));
-//echo '</pre>';
+// Upload API
+$upload = new Uploader();
+echo '<pre>';
+print_r($upload::upload("https://cloudinary-training.github.io/cld-php-migration/images/cloudinary_icon_blue.png",
+['public_id'=>'cloudinary_icon_blue']));
+echo '</pre>';
 
 
 // Admin API
-//use Cloudinary\Api;
-//$api = new Api();
-//echo '<pre>';
-//print_r($api->resources());
-//echo '</pre>';
+$api = new Api();
+echo '<pre>';
+print_r($api->resources());
+echo '</pre>';
 
-//$url =  cloudinary_url(
-//    "sample",
-//    [
-//        "secure" => true,
-//        "transformation" => [
-//            ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
-//            ["radius" => 20],
-//            ["effect" => "sepia"],
-//            [
-//                "overlay" => "cloudinary_icon_blue",
-//                "gravity" => "south_east",
-//                "x"       => 5,
-//                "y"       => 5,
-//                "width"   => 50,
-//                "effect"  => "brightness:200",
-//            ],
-//            ["angle" => 10],
-//        ],
-//    ]
-//);
-//echo $url;
 
-//$image = cl_image_tag(
-//    "sample",
-//    [
-//        "secure" => true,
-//        "transformation" => [
-//            ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
-//            ["radius" => 20],
-//            ["effect" => "sepia"],
-//            [
-//                "overlay" => "cloudinary_icon_blue",
-//                "gravity" => "south_east",
-//                "x"       => 5,
-//                "y"       => 5,
-//                "width"   => 50,
-//                "effect"  => "brightness:200",
-//            ],
-//            ["angle" => 10],
-//        ],
-//    ]
-//);
+$url =  cloudinary_url(
+   "sample",
+   [
+       "secure" => true,
+       "transformation" => [
+           ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
+           ["radius" => 20],
+           ["effect" => "sepia"],
+           [
+               "overlay" => "cloudinary_icon_blue",
+               "gravity" => "south_east",
+               "x"       => 5,
+               "y"       => 5,
+               "width"   => 50,
+               "effect"  => "brightness:200",
+           ],
+           ["angle" => 10],
+       ],
+   ]
+);
+echo $url;
+echo '<br>';
 
-//echo $image;
+$image = cl_image_tag(
+   "sample",
+   [
+       "secure" => true,
+       "transformation" => [
+           ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
+           ["radius" => 20],
+           ["effect" => "sepia"],
+           [
+               "overlay" => "cloudinary_icon_blue",
+               "gravity" => "south_east",
+               "x"       => 5,
+               "y"       => 5,
+               "width"   => 50,
+               "effect"  => "brightness:200",
+           ],
+           ["angle" => 10],
+       ],
+   ]
+);
 
-//echo '<pre>';
-//echo $image;
-//echo '</pre>';
+
+echo $image;
 
 ?>
 </body>
--- index-v2.php
+++ index-v2.php
@@ -8,77 +8,81 @@ echo '<p>SDK2: Uncomment Code to see more</p>';
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use \Cloudinary\Configuration\Configuration;
-// Configuration::instance(['account' => ['cloud_name' => 'CLOUD_NAME', 'api_key' => 'API_KEY', 'api_secret' => 'API_SECRET']]);
-//Configuration::instance();
+use Cloudinary\Asset\Media;
+use Cloudinary\Configuration\Configuration;
+use Cloudinary\Api\Upload\UploadApi;
+use Cloudinary\Api\Admin\AdminApi;
+
+
+Configuration::instance(['account' => ['cloud_name' => 'dec-2020-test', 'api_key' => '552265519859882', 'api_secret' => 'zrs73txUXMnsO93hWxeiRWpTOFw']]);
+echo Configuration::instance()->account->cloudName;
 
 // Upload API
-//use Cloudinary\Api\Upload\UploadApi;
-//$upload = new UploadApi();
+$upload = new UploadApi();
 //
-//echo '<pre>';
-//echo json_encode($upload->upload('https://cloudinary-training.github.io/cld-php-migration/images/cloudinary_icon_blue.png'),JSON_PRETTY_PRINT);
-//echo '</pre>';
+echo '<pre>';
+echo json_encode($upload->upload('https://cloudinary-training.github.io/cld-php-migration/images/cloudinary_icon_blue.png'),JSON_PRETTY_PRINT);
+echo '</pre>';
 
 // Admin api
-//use Cloudinary\Api\Admin\AdminApi;
-//$api = new AdminApi();
+$api = new AdminApi();
 
-//echo '<pre>';
-//echo json_encode($api->resource('sample'),JSON_PRETTY_PRINT);
-//echo '</pre>';
+echo '<pre>';
+echo json_encode($api->resource('sample'),JSON_PRETTY_PRINT);
+echo '</pre>';
 
-//use Cloudinary\Asset\Media;
-//$url = Media::fromParams(
-//    "sample",
-//    [
-//        "secure" => true,
-//        "transformation" => [
-//            ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
-//            ["radius" => 20],
-//            ["effect" => "sepia"],
-//            [
-//                "overlay" => "cloudinary_icon_blue",
-//                "gravity" => "south_east",
-//                "x"       => 5,
-//                "y"       => 5,
-//                "width"   => 50,
-//                "opacity" => 60,
-//                "effect"  => "brightness:200",
-//            ],
-//            ["angle" => 10],
-//        ],
-//    ]
-//);
-//echo $url;
 
-//use Cloudinary\Tag\ImageTag;
-////
-//$image = ImageTag::fromParams('sample',[
-//    "secure" => true,
-//    "transformation" => [
-//        ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
-//        ["radius" => 20],
-//        ["effect" => "sepia"],
-//        [
-//            "overlay" => "cloudinary_icon_blue",
-//            "gravity" => "south_east",
-//            "x"       => 5,
-//            "y"       => 5,
-//            "width"   => 50,
-//            "opacity" => 60,
-//            "effect"  => "brightness:200",
-//        ],
-//        ["angle" => 10],
-//    ]
-//]);
-//
-//echo $image;
+$url = Media::fromParams(
+    "sample",
+    [
+        "secure" => true,
+        "transformation" => [
+            ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
+            ["radius" => 20],
+            ["effect" => "sepia"],
+            [
+                "overlay" => "cloudinary_icon_blue",
+                "gravity" => "south_east",
+                "x"       => 5,
+                "y"       => 5,
+                "width"   => 50,
+                "opacity" => 60,
+                "effect"  => "brightness:200",
+            ],
+            ["angle" => 10],
+        ],
+    ]
+);
+echo $url;
 
+use Cloudinary\Tag\ImageTag;
+/**
+ * @var ImageTag $image
+ */
+$image = ImageTag::fromParams('sample',[
+    "secure" => true,
+    "transformation" => [
+        ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
+        ["radius" => 20],
+        ["effect" => "sepia"],
+        [
+            "overlay" => "cloudinary_icon_blue",
+            "gravity" => "south_east",
+            "x"       => 5,
+            "y"       => 5,
+            "width"   => 50,
+            "opacity" => 60,
+            "effect"  => "brightness:200",
+        ],
+        ["angle" => 10],
+    ]
+]);
+
+echo $image;
+echo '<br>';
 
 // v2 transformation example
-//$image->scale(100);
-//echo $image;
+echo $image->scale(100);
 
 ?>
 </body>
