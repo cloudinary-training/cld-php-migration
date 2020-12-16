928a25f1848dadb9d1b4afc2293b99546855fbb7
Constantine Nathanson <constantine@cloudinary.com>
2020-12-16 01:42:10 -0500

    Fix indentation and small warnings

--- index-v1.php
+++ index-v1.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="HTML5">
 <head>
     <title>PHP Test</title>
 </head>
@@ -8,21 +8,29 @@ echo '<h1>Cloudinary SDK V1</h1>';
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use \Cloudinary\Uploader;
-use \Cloudinary\Api;
+use Cloudinary\Api;
+use Cloudinary\Uploader;
 
-Cloudinary::config(array(
-    'cloud_name' => 'CLOUD_NAME',
-    'api_key' => 'API_KEY',
-    'api_secret' => 'API_SECRET'
-));
+//Cloudinary::config(
+//    [
+//        'cloud_name' => 'CLOUD_NAME',
+//        'api_key'    => 'API_KEY',
+//        'api_secret' => 'API_SECRET',
+//    ]
+//);
+
+echo Cloudinary::config()["cloud_name"];
 
 // Upload API
 echo '<h2>Upload API Response</h2>';
 $upload = new Uploader();
 echo '<pre>';
-print_r($upload::upload("https://cloudinary-training.github.io/cld-php-migration/images/cloudinary_icon_blue.png",
-['public_id'=>'cloudinary_icon_blue']));
+print_r(
+    $upload::upload(
+        "https://cloudinary-training.github.io/cld-php-migration/images/cloudinary_icon_blue.png",
+        ['public_id' => 'cloudinary_icon_blue']
+    )
+);
 echo '</pre>';
 
 
@@ -30,53 +38,53 @@ echo '</pre>';
 echo '<h2>Admin API Response </h2>';
 $api = new Api();
 echo '<pre>';
-echo json_encode($api->resources(["max_results" => 1]),JSON_PRETTY_PRINT);
+echo json_encode($api->resources(["max_results" => 1]), JSON_PRETTY_PRINT);
 echo '</pre>';
 
 
-$url =  cloudinary_url(
-   "sample",
-   [
-       "secure" => true,
-       "transformation" => [
-           ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
-           ["radius" => 20],
-           ["effect" => "sepia"],
-           [
-               "overlay" => "cloudinary_icon_blue",
-               "gravity" => "south_east",
-               "x"       => 5,
-               "y"       => 5,
-               "width"   => 50,
-               "effect"  => "brightness:200",
-           ],
-           ["angle" => 10],
-       ],
-   ]
+$url = cloudinary_url(
+    "sample",
+    [
+        "secure"         => true,
+        "transformation" => [
+            ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
+            ["radius" => 20],
+            ["effect" => "sepia"],
+            [
+                "overlay" => "cloudinary_icon_blue",
+                "gravity" => "south_east",
+                "x"       => 5,
+                "y"       => 5,
+                "width"   => 50,
+                "effect"  => "brightness:200",
+            ],
+            ["angle" => 10],
+        ],
+    ]
 );
 echo '<h2>Cloudinary URL</h2>';
 echo $url;
 echo '<br>';
 
 $image = cl_image_tag(
-   "sample",
-   [
-       "secure" => true,
-       "transformation" => [
-           ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
-           ["radius" => 20],
-           ["effect" => "sepia"],
-           [
-               "overlay" => "cloudinary_icon_blue",
-               "gravity" => "south_east",
-               "x"       => 5,
-               "y"       => 5,
-               "width"   => 50,
-               "effect"  => "brightness:200",
-           ],
-           ["angle" => 10],
-       ],
-   ]
+    "sample",
+    [
+        "secure"         => true,
+        "transformation" => [
+            ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
+            ["radius" => 20],
+            ["effect" => "sepia"],
+            [
+                "overlay" => "cloudinary_icon_blue",
+                "gravity" => "south_east",
+                "x"       => 5,
+                "y"       => 5,
+                "width"   => 50,
+                "effect"  => "brightness:200",
+            ],
+            ["angle" => 10],
+        ],
+    ]
 );
 
 echo '<h2>Cloudinary Image</h2>';
--- index-v2.php
+++ index-v2.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="HTML5">
 <head>
     <title>PHP Test</title>
 </head>
@@ -8,18 +8,21 @@ echo '<h1>Cloudinary SDK V2</h1>';
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Cloudinary\Api\Admin\AdminApi;
+use Cloudinary\Api\Upload\UploadApi;
 use Cloudinary\Asset\Media;
 use Cloudinary\Configuration\Configuration;
-use Cloudinary\Api\Upload\UploadApi;
-use Cloudinary\Api\Admin\AdminApi;
+use Cloudinary\Tag\ImageTag;
 
-
-Configuration::instance(['account' => [
-    'cloud_name' => 'CLOUD_NAME', 
-    'api_key' => 'API_KEY', 
-    'api_secret' => 'API_SECRET'
-    ]
-]);
+//Configuration::instance(
+//    [
+//        'account' => [
+//            'cloud_name' => 'CLOUD_NAME',
+//            'api_key'    => 'API_KEY',
+//            'api_secret' => 'API_SECRET',
+//        ],
+//    ]
+//);
 echo Configuration::instance()->account->cloudName;
 
 // Upload API
@@ -27,7 +30,10 @@ echo '<h2>Upload API</h2>';
 $upload = new UploadApi();
 //
 echo '<pre>';
-echo json_encode($upload->upload('https://cloudinary-training.github.io/cld-php-migration/images/cloudinary_icon_blue.png'),JSON_PRETTY_PRINT);
+echo json_encode(
+    $upload->upload('https://cloudinary-training.github.io/cld-php-migration/images/cloudinary_icon_blue.png'),
+    JSON_PRETTY_PRINT
+);
 echo '</pre>';
 
 // Admin api
@@ -35,14 +41,14 @@ echo '<h2>Admin API</h2>';
 $api = new AdminApi();
 
 echo '<pre>';
-echo json_encode($api->resources(['max_results'=>1]),JSON_PRETTY_PRINT) . "\n";
+echo json_encode($api->resources(['max_results' => 1]), JSON_PRETTY_PRINT) . "\n";
 echo '</pre>';
 
 echo '<h2>Cloudinary URL</h2>';
 $url = Media::fromParams(
     "sample",
     [
-        "secure" => true,
+        "secure"         => true,
         "transformation" => [
             ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
             ["radius" => 20],
@@ -62,28 +68,30 @@ $url = Media::fromParams(
 );
 echo $url;
 
-use Cloudinary\Tag\ImageTag;
 /**
  * @var ImageTag $image
  */
-$image = ImageTag::fromParams('sample',[
-    "secure" => true,
-    "transformation" => [
-        ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
-        ["radius" => 20],
-        ["effect" => "sepia"],
-        [
-            "overlay" => "cloudinary_icon_blue",
-            "gravity" => "south_east",
-            "x"       => 5,
-            "y"       => 5,
-            "width"   => 50,
-            "opacity" => 60,
-            "effect"  => "brightness:200",
+$image = ImageTag::fromParams(
+    'sample',
+    [
+        "secure"         => true,
+        "transformation" => [
+            ["width" => 150, "height" => 150, "gravity" => "face", "crop" => "thumb"],
+            ["radius" => 20],
+            ["effect" => "sepia"],
+            [
+                "overlay" => "cloudinary_icon_blue",
+                "gravity" => "south_east",
+                "x"       => 5,
+                "y"       => 5,
+                "width"   => 50,
+                "opacity" => 60,
+                "effect"  => "brightness:200",
+            ],
+            ["angle" => 10],
         ],
-        ["angle" => 10],
     ]
-]);
+);
 
 echo '<h2>Cloudinary Image</h2>';
 echo $image;
