07ea1136d05732599f6b50a9a4d96149f8d7428b
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-03-30 09:22:59 -0700

    add cloud key to config

--- config-v2.sample.php
+++ config-v2.sample.php
@@ -4,8 +4,11 @@ use Cloudinary\Configuration\Configuration;
 
 Cloudinary::config(
  [
-  'cloud_name' => 'CLOUD_NAME',
-  'api_key'    => 'API_KEY',
-  'api_secret' => 'API_SECRET',
+   "cloud" =>
+   [
+    'cloud_name' => 'CLOUD_NAME',
+    'api_key'    => 'API_KEY',
+    'api_secret' => 'API_SECRET'
+   ]
  ]
 );
